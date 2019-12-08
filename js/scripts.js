4bf0221252a8e71bde9f5103f6196c8fc72ca592
JadyVella <felixouma379@gmail.com>
2019-12-08 23:34:41 +0300

    Add toggle to the elements in what we do h1 tag

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
 $(document).ready(function() {
     $("#design").on("click",function() {
         $(".designAlign").toggle();
+        $("#design").hide();
+        $(".designAlign").on("click",function() {
+            $("#design").show();
+            $(".designAlign").hide();
+        });
     });
 
     $("#dev").on("click",function() {
         $(".devAlign").toggle();
+        $("#dev").hide();
+        $(".devAlign").on("click",function() {
+            $("#dev").show();
+            $(".devAlign").hide();
+        });
+    });
+
+    $("#product").on("click",function() {
+        $(".proAlign").toggle();
+        $("#product").hide();
+        $(".proAlign").on("click",function() {
+            $("#product").show();
+            $(".proAlign").hide();
+        });
     });
 });
